37a4cf3f98331031da39faaae45c0d6651c83c11
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2023-03-19 11:50:45 +0900

    Add files via upload

---
+++ 20230319/Letter.java
@@ -0,0 +1,12 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String name = "a";
+		System.out.println("Hello, "+name+" ... "+name+" .. egoing .. bye");
+
+		double VAT = 10.0; // 다른사람이나 미래의 내가 볼때 의도를 빨리 파악할 수 있게 작성하는게 중요하다
+		System.out.println(VAT);
+	}
+
+}
